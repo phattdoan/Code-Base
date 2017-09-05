d217e1ac0359a4edd1831d7b0f579c90935f798c
phattdoan <phat.t.doan@gmail.com>
2017-09-05 17:21:01 -0600

    add more functions

--- R/R_Functions.r
+++ R/R_Functions.r
@@ -22,3 +22,71 @@ fn_bin_vector = function(vec, bin){
   }
   return(bin.sum)
 }
+
+-----------------------------------------------------------------------------------------------
+## Fucntion to calculate distance between 2 geocodes
+####################################
+```{r}
+get_geo_distance = function(lon1, lat1, lon2, lat2, units = "km") {
+  distance_Haversine = distm(c(lon1, lat1), c(lon2, lat2), fun = distHaversine)
+  
+  if (units == "km") {
+    distance = distance_Haversine  / 1000.0
+  }
+  else if (units == "miles") {
+    distance = distance_Haversine / 1609.344
+  }
+  #else if (units == "meters"){
+  #  distance = distance_Haversine  / 1000000.0
+  #}
+  else {
+    distance = distance_Haversine
+    # This will return in meter as same way as distHaversine function. 
+  }
+  distance
+}
+
+#distance between one grid
+grid_length = get_geo_distance(-113, 42, -112.5, 41.5, "km")
+
+print("Grid length in km: ")
+print(grid_length[1])
+
+#QA Script to test the function
+#test = get_geo_distance(members$Longitude[1], members$Latitude[1], 
+#                        centers$lon[1], centers$lat[1], 'meters')
+
+#test[1]
+
+```
+
+## Calculate distance of each member to each clinic
+```{r}
+for (i in 1:length(centers$name.cleaned)){
+  name = centers$name.cleaned[i]
+  members.15.over65 <- cbind(members.15.over65, name)
+  members.15.over65$name = 0
+  print(centers$Name[i])
+  for (j in 1:length(members.15.over65$Latitude)){
+     temp =  get_geo_distance(centers$lon[i], centers$lat[i], 
+                                           members.15.over65$Longitude[j],
+                                           members.15.over65$Latitude[j],
+                                           "meters")
+     members.15.over65$name[j] = temp[1]
+  }
+  colnames(members.15.over65)[(names(members.15.over65)) == "name"] = paste(centers$name.cleaned[i],
+                                                        "distance",sep=".")
+}
+
+colnames(members.15.over65)
+
+# QA Script
+#centers$lon[1]
+#centers$lat[1] 
+#members.15$Latitude[1]
+#members.15$Longitude[1]
+#print(get_geo_distance(centers$lon[1], centers$lat[1], 
+#                                           members.15$Longitude[1],
+#                                           members.15$Latitude[1],
+#                                           "meters"))
+```
--- R/R_Visualization.r
+++ R/R_Visualization.r
@@ -27,3 +27,31 @@ ggplot(dat, aes(x=defect)) +
   geom_bar(aes(y=count), fill="blue", stat="identity") +
   geom_point(aes(y=cum)) +
   geom_path(aes(y=cum, group=1))
+
+-----------------------------------------------------------------------------------------------
+## Loop to plot
+for (i in 3:length(clinics.diseases.pcnt)){
+  title = paste("Percentage of Members with", colnames(clinics.diseases.pcnt[i]),
+              sep =" ")
+  print(ggplot(data=clinics.diseases.pcnt, aes(x= Clinic, 
+                                               y= clinics.diseases.pcnt[[i]], 
+                                               color = Clinic)) +
+  geom_bar(stat="identity") + coord_flip() +
+  ggtitle(title) +
+  theme(legend.position="none") +
+  labs(y= colnames(clinics.diseases.pcnt[i])))
+}
+
+-----------------------------------------------------------------------------------------------
+## line histogram w/ ggplot for multiple clinics
+ggplot(data = m15.trsf, aes(CC_WEIGHT_VAL, ..density.., colour = Clinic)) + 
+  geom_freqpoly(binwidth = 1) + 
+  labs(title="Histogram for CC_DISEASE_CNT") +
+  labs(x= "CC_DISEASE_CNT", y="Frequency")
+
+-----------------------------------------------------------------------------------------------
+## Spatial mapping with ggmap
+slc.map = get_map('salt lake', scale = 2)
+ggmap(slc.map, extent = 'device')  +
+geom_point(data=centers.geo, aes(x = lon, y = lat),
+            color="red") 
